faa324c26793b78e5c20f3670055c1fe830469b8
Logan Blue <blue-logan@users.noreply.github.com>
2022-02-12 16:03:37 -0500

    Delete code directory

--- code/sph_to_wav.py
+++
@@ -1,6 +0,0 @@
-from sphfile import SPHFile
-sph =SPHFile('../../../Data_Stores/Timit/Data/timit/TIMIT/TRAIN/DR1/FCJF0/SA1.WAV')
-# Note that the following loads the whole file into ram
-print( sph.format )
-# write out a wav file with content from 111.29 to 123.57 seconds
-sph.write_wav( 'timit_test.wav', 111.29, 123.57 )
